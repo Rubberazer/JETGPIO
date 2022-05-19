9ae7a2b125334092e9f24a01892b0c95b6a10fb7
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-05-19 12:45:44 +0200

    Delete make.sh

--- make.sh
+++
@@ -1,6 +0,0 @@
-gcc -c -Wall -fpic jetgpio.c 
-gcc -shared -o libjetgpio.so jetgpio.o
-sudo cp ./libjetgpio.so /usr/lib/
-sudo chmod 0755 /usr/lib/libjetgpio.so
-sudo ldconfig
-sudo ldconfig -p | grep libjetgpio.so
