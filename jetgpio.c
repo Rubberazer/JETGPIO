2e1a8c8cba6c15381bab097319a2f73a8734cdc9
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-10-10 22:40:00 +0200

    Update jetgpio.c

--- jetgpio.c
+++ jetgpio.c
@@ -21,7 +21,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* jetgpio version 0.5 */
+/* jetgpio version 0.7 */
 
 #include <stdio.h>
 #include <stdlib.h>
