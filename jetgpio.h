8653c0d70f13e15152cfcd33809f03fe954f5ae9
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-11-05 21:22:57 +0100

    Version 0.91

--- jetgpio.h
+++ jetgpio.h
@@ -21,7 +21,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* jetgpio version 0.9 */
+/* jetgpio version 0.91 */
 
 #ifndef jetgpio_h__
 #define jetgpio_h__
