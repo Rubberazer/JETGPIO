100bff6c6f31f5f72c84fcda129eebec3b8cae03
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-11-25 02:45:31 +0100

    version 0.94

---
+++ doc/html/search/all_4.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['f',['f',['../structISRFunc.html#a14dc90f579252e93fd6d63fde3cf317e',1,'ISRFunc']]],
+  ['falling_5fedge',['FALLING_EDGE',['../jetgpio_8h.html#a159ba045d627097f4f2d351bec51afe1',1,'jetgpio.h']]],
+  ['fd',['fd',['../structi2cInfo__t.html#a7953e8f6706b1b92ac275fd61e6e145d',1,'i2cInfo_t::fd()'],['../structSPIInfo__t.html#a251aec2a9e6df318135c8f35180334a1',1,'SPIInfo_t::fd()']]],
+  ['flags',['flags',['../structi2cInfo__t.html#a90fde1a7bf5b6939fd75d5b56a9023a1',1,'i2cInfo_t']]],
+  ['funcs',['funcs',['../structi2cInfo__t.html#a91af064c970230d0119d7220861fed32',1,'i2cInfo_t']]]
+];
---
+++ doc/html/search/defines_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['either_5fedge',['EITHER_EDGE',['../jetgpio_8h.html#aff4e22f0a6c14fdc9ca8e6ce97e803de',1,'jetgpio.h']]]
+];
