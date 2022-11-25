aa5124cf397faf039de650f365273007f6962093
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-11-25 12:03:51 +0100

    Delete docs/hmtl directory

--- docs/hmtl/search/all_7.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['jetgpio_20library',['JETGPIO library',['../index.html',1,'']]],
-  ['jet_5finput',['JET_INPUT',['../jetgpio_8h.html#aa5456a9b82ea8b5dc5e79c069dc29a7b',1,'jetgpio.h']]],
-  ['jet_5foutput',['JET_OUTPUT',['../jetgpio_8h.html#ad589cca7b5afd4362dede39fc9af7159',1,'jetgpio.h']]],
-  ['jetgpio_2ec',['jetgpio.c',['../jetgpio_8c.html',1,'']]],
-  ['jetgpio_2eh',['jetgpio.h',['../jetgpio_8h.html',1,'']]]
-];
--- docs/hmtl/search/variables_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['edge',['edge',['../structISRFunc.html#a2704ac2052bd84de5123140f083d13c5',1,'ISRFunc']]]
-];
