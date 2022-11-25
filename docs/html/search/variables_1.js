2a7dda603f480aa187ec343f3bd7db4ab4f913bd
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2022-11-25 12:08:19 +0100

    Add files via upload

---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['bits_5fword',['bits_word',['../structSPIInfo__t.html#a87c14adb9fdabd30200053e1af093f53',1,'SPIInfo_t']]]
+];
---
+++ docs/html/search/variables_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gpio',['gpio',['../structISRFunc.html#ad128a8b80cb18275d612476b68b4014e',1,'ISRFunc']]],
+  ['gpio_5foffset',['gpio_offset',['../structISRFunc.html#ab4159dafce6069b82aea681e69d1412c',1,'ISRFunc']]]
+];
