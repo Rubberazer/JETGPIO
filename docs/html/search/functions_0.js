80761ed961a4dcddd3f272225a8e480e415f77ad
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2023-01-18 22:18:05 +0100

    Add files via upload

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['callback',['callback',['../jetgpio_8c.html#afbdda01d8935cc2c2826747200b369cf',1,'jetgpio.c']]]
+];
---
+++ docs/html/search/variables_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cnf',['CNF',['../structGPIO__CNF.html#ab32fd3f6806424b780178778dc0eeef4',1,'GPIO_CNF']]],
+  ['cs_5fchange',['cs_change',['../structSPIInfo__t.html#affb9e38d1dde93e7a11bcd08bc247c6c',1,'SPIInfo_t']]],
+  ['cs_5fdelay',['cs_delay',['../structSPIInfo__t.html#a4bbee2a7dc7cdc1fd174fe34a4bf3ced',1,'SPIInfo_t']]]
+];
