47f513d68f011d5f9fe80f605278b252559cf2bb
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2023-01-18 22:11:12 +0100

    Delete docs/html/search directory

--- docs/html/search/defines_6.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jet_5finput',['JET_INPUT',['../jetgpio_8h.html#aa5456a9b82ea8b5dc5e79c069dc29a7b',1,'jetgpio.h']]],
-  ['jet_5foutput',['JET_OUTPUT',['../jetgpio_8h.html#ad589cca7b5afd4362dede39fc9af7159',1,'jetgpio.h']]]
-];
--- docs/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['addr',['addr',['../structi2cInfo__t.html#a40a68b77f11a0b1c73aa615dd4800201',1,'i2cInfo_t']]]
-];
