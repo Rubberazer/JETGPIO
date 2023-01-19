a91ee349ca53dc03d3f6848cc88d293e6f33ef77
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2023-01-19 19:00:18 +0100

    Add files via upload

---
+++ docs/html/search/variables_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['f',['f',['../structISRFunc.html#a14dc90f579252e93fd6d63fde3cf317e',1,'ISRFunc']]],
+  ['fd',['fd',['../structi2cInfo__t.html#a7953e8f6706b1b92ac275fd61e6e145d',1,'i2cInfo_t::fd()'],['../structSPIInfo__t.html#a251aec2a9e6df318135c8f35180334a1',1,'SPIInfo_t::fd()']]],
+  ['flags',['flags',['../structi2cInfo__t.html#a90fde1a7bf5b6939fd75d5b56a9023a1',1,'i2cInfo_t']]],
+  ['funcs',['funcs',['../structi2cInfo__t.html#a91af064c970230d0119d7220861fed32',1,'i2cInfo_t']]]
+];
