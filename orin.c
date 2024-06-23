ac0ee820f8151cb44cf16c93139d3868b94c3c4c
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2024-06-24 00:06:23 +0200

    adding supoprt for Orin AGX

--- orin.c
+++ orin.c
@@ -2211,7 +2211,7 @@ int gpioPWM(unsigned gpio, unsigned dutycycle) {
       break;
     default:
       status = -1;
-      printf("Only gpio numbers 32 and 33 are accepted,\n");
+      printf("Only gpio numbers 15, 32 and 33 are accepted,\n");
     }
   }
   else {printf("Only a dutycycle from 0 to 256 is allowed\n");
